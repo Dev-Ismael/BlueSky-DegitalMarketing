7fa249e7da92566e8ae38d696a545ff14466af23
unknown <dev.ismael305@gmail.com>
2022-08-23 16:30:46 +0200

    add About section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,13 @@
     <!---------- Header ------------>
     <section class="hero-equal-height ptb-70">
         <div class="hero-shape" style="background: url('{{ asset("digimark_template/img/Header-01.svg") }}')no-repeat top right"></div>
-        <img src="{{ asset("digimark_template/img/cta-2-path-shape.png") }}" alt="group shape" class="img-fluid group-shape-1">
+        {{-- <img src="{{ asset("digimark_template/img/cta-2-path-shape.png") }}" alt="group shape" class="img-fluid group-shape-1">
         <img src="{{ asset("digimark_template/img/cta-2-circle-shape.png") }}" alt="group shape" class="img-fluid group-shape-2">
         <img src="{{ asset("digimark_template/img/cta-2-tringle-shape.png") }}" alt="group shape" class="img-fluid group-shape-3">
-        <img src="{{ asset("digimark_template/img/cta-2-tringle-shape.png") }}" alt="group shape" class="img-fluid group-shape-4">
+        <img src="{{ asset("digimark_template/img/cta-2-tringle-shape.png") }}" alt="group shape" class="img-fluid group-shape-4"> --}}
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-md-12 col-lg-6">
+                <div class="col-md-12 col-lg-5">
                     <div class="hero-slider-content pt-5">
                         <strong class="color-secondary">Digital Marketing</strong>
                         <h1>Ready <br> For Growth?</h1>
@@ -31,9 +31,9 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-12 col-lg-6">
+                <div class="col-md-12 col-lg-7">
                     <div class="img-wrap">
-                        <img src="{{ asset("digimark_template/img/t5-hero-img.svg") }}" alt="hero" class="img-fluid custom-width">
+                        <img src="{{ asset("digimark_template/img/final.gif") }}" alt="hero" class="img-fluid custom-width">
                     </div>
                 </div>
             </div>
@@ -42,7 +42,91 @@
 
 
 
-    
+    <!----------  About ------------>
+    <section class="about-with-promo ptb-100 gray-light-bg">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="about-content-wrap">
+                        <strong class="color-secondary">Digital Marketing Services</strong>
+                        <h2>Why Your Business Need Digital Marketing</h2>
+                        <span class="animate-border mb-4"></span>
+                        <p>Globally parallel task quality methodologies whereas inexpensive sources. Uniquely
+                            administrate ubiquitous web services through diverse quality vectors syndicate error-free
+                            intellectual capital. </p>
+
+                        <ul class="check-list-info">
+                            <li><strong>Advance Advisory Team</strong> – often with soap and water for at least 20
+                                seconds especially after you have been in a public place, or after blowing your nose,
+                                coughing, or sneezing.
+                            </li>
+                            <li><strong>Professional Consulting Services</strong> – if spreading in your community. This
+                                is especially important for people who are at higher risk of getting very sick brand future-proof intellectual capital.
+                            </li>
+                            <li><strong>24/7 Support Help Center</strong> – with a tissue when you cough or sneeze or
+                                use the inside of your elbow.
+                            </li>
+                            <li><strong>Customer Service &amp; Operations</strong> – This includes phones, tables, light
+                                switches, doorknobs, countertops, handles, desks, toilets.
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="about-promo-wrap">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-6 promo-col">
+                                <div class="promo-item">
+                                    <div class="promo-img">
+                                        <img src="{{ asset("digimark_template/img/increase_1.gif") }}" alt="services" width="60">
+                                    </div>
+                                    <h5>Increased Traffic</h5>
+                                    <p>Wash your hands frequently more then 20s</p>
+                                    <div class="promo-bottom-shape">
+                                        <img src="{{ asset("digimark_template/img/promo-box-shape-bottom.png") }}" alt="shape">
+                                    </div>
+                                </div>
+                                <div class="promo-item">
+                                    <div class="promo-img">
+                                        <img src="{{ asset("digimark_template/img/search-engine_188.gif") }}" alt="services" width="60">
+                                    </div>
+                                    <h5>Site Optimisation</h5>
+                                    <p>Progressively leverage existing resource-leveling methodologies</p>
+                                    <div class="promo-bottom-shape">
+                                        <img src="{{ asset("digimark_template/img/promo-box-shape-bottom.png") }}" alt="shape">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-6 promo-col">
+                                <div class="promo-item">
+                                    <div class="promo-img">
+                                        <img src="{{ asset("digimark_template/img/credit-card.gif") }}" alt="services" width="60">
+                                    </div>
+                                    <h5>Cost-effectiveness</h5>
+                                    <p>If you have fever, cough and difficulty breathing, seek medical care early</p>
+                                    <div class="promo-bottom-shape">
+                                        <img src="{{ asset("digimark_template/img/promo-box-shape-bottom.png") }}" alt="shape">
+                                    </div>
+                                </div>
+                                <div class="promo-item">
+                                    <div class="promo-img">
+                                        <img src="{{ asset("digimark_template/img/interface-2.gif") }}" alt="services" width="60">
+                                    </div>
+                                    <h5>Page Rankings</h5>
+                                    <p>Rapidiously integrate viral infrastructures reliable. </p>
+                                    <div class="promo-bottom-shape">
+                                        <img src="{{ asset("digimark_template/img/promo-box-shape-bottom.png") }}" alt="shape">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <!------- Our Clients ----------->
     <div class="client-section ptb-100" style="background: url('{{ asset('digimark_template/img/client-bg.jpg') }}')no-repeat center center / cover">
         <div class="container">
