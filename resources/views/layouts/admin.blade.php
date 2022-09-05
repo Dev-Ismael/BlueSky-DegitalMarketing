646f1cdd94da76640fc0dbdddfec7fd1d66cfd02
unknown <dev.ismael305@gmail.com>
2022-09-05 15:06:12 +0200

    add admin template

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<!-- Mirrored from demo.dashboardpack.com/cryptocurrency-html/data_table.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 05 Sep 2022 12:14:40 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+
+<head>
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>BitCrypto</title>
+    <link rel="icon" href="{{ asset('admin/img/mini_logo.png" type="image/png') }}">
+
+    <link rel="stylesheet" href="{{ asset('admin/css/bootstrap1.min.css') }}"/>
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/themefy_icon/themify-icons.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/scroll/scrollable.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/font_awesome/css/all.min.css') }}" />
+
+    <link rel="stylesheet" href="{{ asset('admin/vendors/datatable/css/jquery.dataTables.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('admin/vendors/datatable/css/responsive.dataTables.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('admin/vendors/datatable/css/buttons.dataTables.min.css') }}" />
+
+
+    <link rel="stylesheet" href="{{ asset('admin/css/metisMenu.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('admin/css/style1.css') }}" />
+</head>
+
+<body class="crm_body_bg">
+
+
+
+
+
+
+
+
+    <div id="app">
+
+
+        @yield('content')
+
+
+
+        <div id="back-top" style="display: none;">
+            <a title="Go to Top" href="#">
+                <i class="ti-angle-up"></i>
+            </a>
+        </div>
+
+
+    </div>
+
+
+
+
+
+    <script src="{{ asset('admin/js/jquery1-3.4.1.min.js') }}"></script>
+    <script src="{{ asset('admin/js/popper1.min.js') }}"></script>
+    <script src="{{ asset('admin/js/bootstrap1.min.js') }}"></script>
+    <script src="{{ asset('admin/js/metisMenu.js') }}"></script>
+    <script src="{{ asset('admin/vendors/datatable/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('admin/vendors/datatable/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('admin/vendors/datatable/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('admin/vendors/datatable/js/buttons.flash.min.js') }}"></script>
+    <script src="{{ asset('admin/vendors/datatable/js/jszip.min.js') }}"></script>
+    <script src="{{ asset('admin/vendors/datatable/js/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('admin/vendors/datatable/js/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('admin/vendors/datatable/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('admin/vendors/datatable/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('admin/vendors/scroll/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('admin/vendors/scroll/scrollable-custom.js') }}"></script>
+    <script src="{{ asset('admin/js/custom.js') }}"></script>
+
+
+</body>
+
+<!-- Mirrored from demo.dashboardpack.com/cryptocurrency-html/data_table.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 05 Sep 2022 12:14:40 GMT -->
+
+</html>
